6b939f6f7b22574018e7912fc908eda783be25a1
Merlin Cox <merlin.cox@denma.digital>
2019-02-03 18:31:30 +0000

    Better error handling

--- api/main.go
+++ api/main.go
@@ -26,31 +26,34 @@ func main() {
 	log.Printf("Starting %v API using Go %v\n", os.Getenv("RELEASE"), runtime.Version())
 	log.Printf("Commit %v Timestamp %v\n", os.Getenv("COMMIT"), os.Getenv("TIMESTAMP"))
 
-	status := models.Status{
-		Platform:  os.Getenv("PLATFORM"),
-		Commit:    os.Getenv("COMMIT"),
-		Branch:    os.Getenv("BRANCH"),
-		Release:   os.Getenv("RELEASE"),
-		Timestamp: time.Now().Format(time.RFC3339Nano),
-	}
-
 	dbi, apiErr := db.NewDbi(os.Getenv("MYSQLDSN"), nil)
 
 	if apiErr != nil {
 
+		log.Printf("Fatal database error: %v", apiErr.Error())
+
 		handler = func(request events.APIGatewayProxyRequest) (response events.APIGatewayProxyResponse, err error) {
 
-			body := apiErr.ErrorBody()
-			body.Code = http.StatusServiceUnavailable
+			body := models.ConstructApiError(http.StatusServiceUnavailable, "Fatal database error: %v", apiErr.Error())
 
 			response = events.APIGatewayProxyResponse{
 				StatusCode: http.StatusServiceUnavailable,
 				Body:       utils.JsonStringify(body),
 			}
+
 			return
 		}
 
 	} else {
+
+		status := models.Status{
+			Platform:  os.Getenv("PLATFORM"),
+			Commit:    os.Getenv("COMMIT"),
+			Branch:    os.Getenv("BRANCH"),
+			Release:   os.Getenv("RELEASE"),
+			Timestamp: time.Now().Format(time.RFC3339Nano),
+		}
+
 		handler = front.NewFront(dbi, status, cacheTtlSeconds).Handler
 	}
 
