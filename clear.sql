182ed132f38285446bf5c11aac197ee2913102a4
Merlin Cox <merlin.cox@denma.digital>
2019-02-04 13:55:46 +0000

    DB build utility

--- clear.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE auth_movements;
-DROP TABLE movements;
-DROP TABLE authorisations;
-DROP TABLE cards;
-DROP TABLE customers;
-DROP TABLE vendors;
-
--- rebuild_db.sh
+++ rebuild_db.sh
@@ -1,3 +1,46 @@
+#!/usr/bin/env bash
+
+cd "$( dirname "$0" )"
+
+for cmd in "mysql"; do
+
+    if [[ -z "$(which ${cmd})" ]]; then
+        echo "${cmd} is required to run this script."  >&2
+        exit 1
+    fi
+
+done
+
+# The mysql.sh script contain DB connection details and should be git-ignored
+mysql_script=mysql.sh
+
+if [[ ! -f ${mysql_script} ]]; then
+   echo "${mysql_script} is required to run this script."  >&2
+   echo "See ${mysql_script}.example for an example."  >&2
+
+   exit 1
+fi
+
+source ${mysql_script}
+
+read -p "About to rebuild the ${mysql_db} database on ${mysql_host}, dropping and recreating all API tables. Confirm? :" -n 1 -r reply
+echo
+case "$reply" in
+    y|Y ) echo "Proceeding with rebuild...";;
+    * )   echo "Cancelling rebuild" >&2
+          exit 1
+          ;;
+esac
+
+mysql -h "${mysql_host}" -u "${mysql_user}" "-p${mysql_passwd}" "${mysql_db}" <<!!!
+
+DROP TABLE IF EXISTS auth_movements;
+DROP TABLE IF EXISTS movements;
+DROP TABLE IF EXISTS authorisations;
+DROP TABLE IF EXISTS cards;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS vendors;
+
 CREATE TABLE IF NOT EXISTS customers (
   id       INT          NOT NULL AUTO_INCREMENT,
   fullname VARCHAR(256) NOT NULL,
@@ -106,8 +149,11 @@ INSERT INTO customers (fullname)
 VALUES ('John Smith'),('Jane Doe');
 
 INSERT INTO vendors (vendor_name)
-VALUES('Coffee Shop 1'),('Supermarket 1'),('Pub 1');
+VALUES('Coffee Shop'),('Supermarket'),('Pub');
 
 INSERT INTO cards (customer_id, balance, available)
 VALUES(1001, 100000, 100000);
 
+SHOW TABLES;
+
+!!!
